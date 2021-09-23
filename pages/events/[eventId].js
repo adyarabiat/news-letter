ad417c3d93b21b176134bd1892957214c68ed935
adyarabiat <ady.arabiat@gmail.com>
2021-09-23 22:31:31 +0300

    minor fix

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -6,7 +6,7 @@ import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
-
+import Comment from "../../components/input/comments";
 function EventDetailPage(props) {
    const event = props.selectedEvent;
 
